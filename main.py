f4f0fa8a41eeee7fee2eaf4a7fa38273de75991d
Aiman Kassam <aimankassam2004@gmail.com>
2024-01-29 19:17:53 -0500

    Created a lists of frames based on action, stored action lists in overhead animation list

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-import pygame
+#Personal project utilizing Pygame to create a platformer game. I use royalty-free tilesets and sprites found online. This project serves as a way to practice OOP principles, while also creating something fun.
 
+#Spritesheet created by @ScissorMarks on Twitter.
+
+import pygame
 from spritesheet import Spritesheet
 
 
-#Global constants
+#Defining global constants for screen width and height to be used later.
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = SCREEN_WIDTH * 9 / 16
 
 
-
+#Initializing Pygame game.
 pygame.init()
 # pygame.display.set_caption("Pokemon")
 
+
+#Defining size of the screen.
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+#Importing and parsing spritesheet into array. Using tick functions to iterate through frames.
 sprite = Spritesheet("dinosprites.png", 24, 24)
 frames = sprite.parseSpritesheet((0,0,0), 2)
 animationCooldown = 100
 currentFrame = 0
-
 lastUpdate = pygame.time.get_ticks()
 
+#Parsing array of all frames into different actions (running, jumping, hurt, etc.). Each index in the animation list will contain a subarray of all frames for one specific action. Each action will be assigned to a number arbitrarily and will be controlled by the variable "action" (action = 0 means the player is currently idle).
+animationList = []
+framesPerActionList = [4, 6, 3, 4]
+action = 0;
+stepCounter = 0
+
+#Assigns a list of frames to each index in animationList.
+for framesPerAction in framesPerActionList:
+    tempImgList = []
+    for _ in range(framesPerAction):
+        tempImgList.append(frames[stepCounter])
+        stepCounter += 1
+    animationList.append(tempImgList)
+   
+    
 running = True
 
 #Game loop
@@ -28,13 +49,16 @@ while running:
 
     screen.fill((0,0,0))
     currentTime = pygame.time.get_ticks()
+
+    #Updating frames for idle animation at frequency of perviously defined animation cooldown.
     if currentTime - lastUpdate >= animationCooldown:
         currentFrame += 1
         lastUpdate = currentTime
-        if currentFrame == 4:
-            currentFrame = 0
+    if currentFrame == len(animationList[0]):
+        currentFrame = 0
+    screen.blit(animationList[2][currentFrame], (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-    screen.blit(frames[currentFrame], (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
--- spritesheet.py
+++ spritesheet.py
@@ -1,5 +1,15 @@
+#Class object dedicated to loading and parsing spritesheet for character animation, and storing individual frames in an array.
+
+#The array parsed will contain all frames within the spritesheet, which can then be separated later into subarrays of frames for idle, movement, jumping, etc.
+
+#For now, this class is designed assuming the spritesheet will only list out frames horizontally, and that all frames are equal size.
+
 import pygame
 
+
+
+
+#Imports spritesheet image file. Width and height will be the dimensions of each individual frame (if each frame is 24x24 pixels, input width = 24, height = 24)
 class Spritesheet:
     def __init__(self, sheet, width, height):
         self.sheet = pygame.image.load(sheet)
@@ -7,7 +17,7 @@ class Spritesheet:
         self.height = height
         self.numberOfFrames = int(self.sheet.get_width() / width)
 
-
+#Internally used method, extracts single frame from a spritesheet and returns it as a Pygame surface. Assuming frames are ordered from left to right, starting at 0, returns frame based on frameNumber input.
     def get_frame(self, frameNumber, color, scale):
         image = pygame.Surface((self.width, self.height)).convert_alpha()
         image.blit(self.sheet, (0,0), (frameNumber * self.width, 0, self.width, self.height))
@@ -16,9 +26,9 @@ class Spritesheet:
 
         return image
 
-
+#Iterates through spritesheet, uses get_frame method to store all frames inside an array and returns the array.
     def parseSpritesheet(self, color, scale=1):
         frames = []
-        for x in range(self.numberOfFrames):
-            frames.append(self.get_frame(x, color, scale))
+        for frame in range(self.numberOfFrames):
+            frames.append(self.get_frame(frame, color, scale))
         return frames
